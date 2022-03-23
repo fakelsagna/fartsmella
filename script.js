95a0d75eed980daf91f759114f95b23a911709d0
Guarf <76407365+fakelsagna@users.noreply.github.com>
2022-03-23 14:07:36 -0400

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ function handleFiles(event) {
     var files = event.target.files;
     $("#src").attr("src", URL.createObjectURL(files[0]));
     document.getElementById("audio").load();
+    console.log(files.volume);
 }
 
 document.getElementById("upload").addEventListener("change", handleFiles, false);
